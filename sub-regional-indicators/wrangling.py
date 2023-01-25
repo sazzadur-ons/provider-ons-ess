7c78e8f58385c2afff77db5bf779316aa696e49a
RedWalters <jonathan.j.walters@Gmail.com>
2023-01-25 16:10:41 +0000

    Add files via upload

---
+++ sub-regional-indicators/wrangling.py
@@ -0,0 +1,29 @@
+import click
+import pandas as pd
+import math
+from pathlib import Path
+
+
+@click.command()
+@click.argument("input", type=click.Path(exists=True, path_type=Path))
+@click.option("--output", default=Path("./output.csv"), type=click.Path(path_type=Path))
+def wrangle(input: Path(), output: Path()) -> None:
+    df = pd.read_csv(input, encoding='ISO-8859-1', skiprows= 2)
+    
+    df = df.replace({'Period' :{201821 : '2018-2021',
+                                201719 : '2017-2019', 
+                                201820 : '2018-2020', 
+                                201920 : '2019-2020',
+                                202021 : '2020-2021',
+                                202122 : '2021-2022'}})
+
+    df['Marker'] = ''
+
+    df['Marker'] = df.apply(lambda x: 'not-available' if math.isnan(x['Value']) else '', axis = 1)
+    
+    df.to_csv(output, index=False)
+    return
+
+
+if __name__ == "__main__":
+    wrangle()
