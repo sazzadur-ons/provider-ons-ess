8240561a3b96c7825251ae2d1ff09f5ff4fdd0e2
RedWalters <jonathan.j.walters@Gmail.com>
2023-01-26 11:36:55 +0000

    don't worry about it

--- sub-regional-indicators/wrangling.py
+++ sub-regional-indicators/wrangling.py
@@ -10,20 +10,7 @@ from pathlib import Path
 def wrangle(input: Path(), output: Path()) -> None:
     df = pd.read_csv(input)
     
-    df = df.replace({'Period' :{201821 : '2018-2021',
-                                201719 : '2017-2019', 
-                                201820 : '2018-2020', 
-                                201920 : '2019-2020',
-                                202021 : '2020-2021',
-                                202122 : '2021-2022'}})
-
-    df['Marker'] = ''
-
-    df['Marker'] = df.apply(lambda x: 'not-available' if math.isnan(x['Value']) else '', axis = 1)
-    df['MAD'] = df.apply(lambda x: '' if pd.isnull(x['MAD']) else x['MAD'], axis = 1)
-
-    indexNames = df[df['Area'].isna()].index
-    df.drop(indexNames, inplace = True)
+    
     
     df.to_csv(output, index=False)
     return
