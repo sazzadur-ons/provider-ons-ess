d8ee622555501f585808b822f34a78af513bf38d
RedWalters <jonathan.j.walters@Gmail.com>
2023-02-02 11:28:59 +0000

    not sure if this wrangling will work but let's see, also not sure if you can still have no measure/unit column. Provider-Forest doesn't but csvcubed complains if I don't

---
+++ gross-disposable-household-income-per-head/wrangling.py
@@ -0,0 +1,43 @@
+import click
+import pandas as pd
+import math
+from pathlib import Path
+
+
+@click.command()
+@click.argument("input", type=click.Path(exists=True, path_type=Path))
+@click.option("--output", default=Path("./output.csv"), type=click.Path(path_type=Path))
+def wrangle(input: Path(), output: Path()) -> None:
+    df = pd.read_csv(input)
+
+    df = df.drop(columns=['AREANM', 'Geography', 'Variable Name', 'Lower Confidence Interval (95%)', 'Upper Confidence Interval (95%)', 'Observation Status', 'Polarity', 'Notes'])
+
+    #Area and Geography are unneeded, Geocode is enough
+    #Variable name contains the name as indicator
+    #Lower and Upper CI are all NA
+    #Observation status are all normal
+    #Polarity is all 1
+    #Notes are all empty, can add any of these back if needed
+
+    df = df.rename(columns= {'AREACD':'Area', 'Value':'Observation'})
+
+    df = df[['Period', 'Area', 'Mission', 'Category', 'Indicator', 'Observation']]
+
+    #Not sure whether to keep in Indicator or Mission
+    #Indicator seems to just be measure type but unsure if to be used to compare datasets
+    #no idea what mission is used for but keeping it in regardless
+
+    #Unsure whether to remove Indicator and include it as the measure in in the config       
+    
+    df.to_csv(output, index=False)
+    return
+
+
+if __name__ == "__main__":
+
+    df = pd.read_excel("2_2023_GDHIperhead.xlsx")
+    df.to_csv('gross-disposable-household-income-per-head.csv', index = False)
+
+    #I don't know whether this will work but it's worth a punt
+
+    wrangle()
